bbddbc0ff1c76fffb9ae27a281ee5fbe2be381b2
phanhuyduong2003 <huyduongphan8@gmail.com>
2023-07-22 21:16:17 +0700

    SCSS

---
+++ js/main.js
@@ -0,0 +1,8 @@
+function openNav() {
+    document.getElementById('sidebar').style.width = '375px';
+    document.body.style.backgroundColor = 'rgba(0,0,0,0.4)';
+}
+function closeNav() {
+    document.getElementById('sidebar').style.width = '0';
+    document.body.style.backgroundColor = 'white';
+}
